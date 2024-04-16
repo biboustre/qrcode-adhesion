0fb39f61468dba6f6c2a1bf874a34b037dd612f8
MAAMAR BENHADJAR <biboustre@hotmail.com>
2024-04-16 17:21:13 +0200

    style button

--- src/app/components/button/page.tsx
+++ src/app/components/button/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../button/page.css';
 
 export default function btn({ text }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./components/header/page";
 import LOGO from "../assets/images/Group 112.png";
 import Event from "../app/components/event/page";
 import '../../src/app/page.css';
+import Image from "next/image";
+import SCAN from '../assets/images/Subtract.png';
+import Btn from './components/button/page'
 
 export default function Home() {
   return (
@@ -10,6 +13,10 @@ export default function Home() {
       <Header logo={LOGO} alt="ok" className="headerHome">
         <Event className="eventHome" title="Merci de scaner le qr code admin pour démarer la session de scan" name="" date="" />
       </Header>
+      <main className="mainHome">
+        <Image src={SCAN} alt="logo de scan" />
+      </main>
+      <Btn text="Mon appareil photo ne marche pas"/>
     </div>
   );
 }
